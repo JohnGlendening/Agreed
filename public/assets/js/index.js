dbda7fb80476c441cd1a838b06638f3f29ec085c
John Glen <jglendening11@gmail.com>
2020-09-25 19:03:09 -0500

    updated buttons

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
     document.getElementById("hostBtn").addEventListener("click", function() {
-        window.location.replace("./../public/host.html");
+        window.location.replace("host.html");
     });
 
     document.getElementById("joinBtn").addEventListener("click", function() {
-        window.location.replace("./../public/login.html");
+        window.location.replace("login.html");
     });
 })
