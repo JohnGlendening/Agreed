5704908ebd2fdd65d7dfc42c299fccc4c4da806d
John Glen <jglendening11@gmail.com>
2020-09-25 19:45:35 -0500

    updated favicons

--- public/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    document.getElementById("hostBtn").addEventListener("click", function() {
-        window.location.replace("./../public/host.html");
-    });
-
-    document.getElementById("joinBtn").addEventListener("click", function() {
-        window.location.replace("./../public/login.html");
-    });
-})
